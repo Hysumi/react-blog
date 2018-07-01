90add9071beade1bd31a94da32eda55551da7cbe
hysumi <henrique.sumitomo@gmail.com>
2018-07-01 17:38:29 -0300

    Configurando Routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import {
+    Route,
+    Link,
+    BrowserRouter as Router
+} from "react-router-dom";
+
+// Containers
+import Home from "./containers/Home/Home";
+import Blog from "./containers/Blog/Blog";
+
+const Topic = ({ match }) => (
+    <div>
+        <h2>{match.params.topicId}</h2>
+    </div>
+);
+
+const Topics = ({ match }) => (
+    <div>
+        <h2>Topics</h2>
+        <ul>
+            <li><Link to={`${match.url}/rendering`}>Rendering with React</Link></li>
+            <li><Link to={`${match.url}/components`}>Components</Link></li>
+            <li><Link to={`${match.url}/props-v-state`}>Props v. State</Link></li>
+        </ul>
+
+        <Route exact path="/topics/:topicId" component={Topic} />
+    </div>
+);
+
 
 class App extends Component {
     render () {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-            </div>
+            <Router>
+                <div>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/about">Blog</Link></li>
+                        <li><Link to="topics">Topics</Link></li>
+                    </ul>
+
+                    <hr />
+                    <Route exact path="/" component={Home} />
+                    <Route path="/about" component={Blog} />
+                    <Route path="/topics" component={Topics} />
+                </div>
+            </Router>
         );
     }
 }
