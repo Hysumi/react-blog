c657418676b67a7a6435fa0c41d65364ef2cf6bb
hysumi <henrique.sumitomo@gmail.com>
2018-07-15 11:49:30 -0300

    Banner: Ajustar imagem

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
-import { BannerBackground } from "./Banner.style";
+import { BannerContainer, BannerImage } from "./Banner.style";
 
 export class Banner extends Component {
 
     render () {
         return (
-            <BannerBackground src={require(`../../assets/images/${this.props.bannerBackgroundImage}`)}>
-            </BannerBackground>
+            <BannerContainer>
+                <BannerImage src={require(`../../assets/images/${this.props.bannerBackgroundImage}`)}/>
+            </BannerContainer>
         );
     }
 }
--- src/components/Banner/Banner.style.js
+++ src/components/Banner/Banner.style.js
@@ -1,11 +1,10 @@
 import styled from "styled-components";
 
-export const BannerBackground = styled.img`
-    /*height: 70%;*/
-    height: 400px;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    position: relative;
-    margin-top: 70px;
+export const BannerContainer = styled.div`
+    padding-top: 4em;
+`;
+
+
+export const BannerImage = styled.img`
+    width:100%;
 `;
