792be12345ab3ccbec7574bb16a2f7bc6efad706
hysumi <henrique.sumitomo@gmail.com>
2018-07-19 09:35:37 -0300

    Criar mock de dados

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -6,50 +6,8 @@ import { Container } from "../../theme/grid";
 import { FeaturedCard } from "../../components/Cards/FeaturedCard/FeaturedCard";
 import { Banner } from "../../components/Banner/Banner";
 
-import { GamesColor, TutorialsColor, BlogColor } from "../../theme/variables";
-
-const Cards = [
-    {
-        Type: "Games",
-        Date: "Jul. 07, 2018",
-        Tags: ["Unity", "Memes"],
-        Title: "Buckat: Ainda lançarei!",
-        SubTitle: "Mas não é hoje...",
-        Content: "Mussum Ipsum, cacilds vidis litro abertis. Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi.",
-        Background: "buckat.jpg",
-        ColorType: GamesColor
-    },
-    {
-        Type: "Tutorials",
-        Date: "Jul. 06, 2018",
-        Tags: ["Unity", "C#", "UI"],
-        Title: "Games - Unity",
-        SubTitle: "Um tutorial ai",
-        Content: "Mussum Ipsum, cacilds vidis litro abertis. Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi.",
-        Background: "unity.jpg",
-        ColorType: TutorialsColor
-    },
-    {
-        Type: "Blog",
-        Date: "Jul. 05, 2018",
-        Tags: ["Music", "OST", "Super Mario 64"],
-        Title: "Relaxing Musics",
-        SubTitle: "Dire Dire Docs",
-        Content: "Mussum Ipsum, cacilds vidis litro abertis. Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi.",
-        Background: "dirediredocs.png",
-        ColorType: BlogColor
-    },
-    {
-        Type: "Blog",
-        Date: "Jul. 05, 2018",
-        Tags: ["Maple Story", "TryHard", "Crack"],
-        Title: "Level 220!!!",
-        SubTitle: "Que plot <3",
-        Content: "Mussum Ipsum, cacilds vidis litro abertis. Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi.",
-        Background: "maplestory.png",
-        ColorType: BlogColor
-    }
-];
+// Data
+import { Cards } from "../../mocks/homepage-data";
 
 export default class Home extends Component {
     render () {
